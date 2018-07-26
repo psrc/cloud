fd003df9c48d653a9345f494a118c0789a2e4472
bricegnichols <bricenichols@gmail.com>
2018-07-26 14:38:44 -0700

    updates git repos for soundcast, urbansim, and urbansim_data

--- cloud_setup.py
+++ cloud_setup.py
@@ -8,30 +8,65 @@ from cloud_setup_config import *
 
 
 def git(*args):
-	"""
+	'''
 	Call Git 
-	"""
+	'''
 	return subprocess.check_call(['git'] + list(args))
 
-# For a given machine
-cloud_name = cloud_list[-1]
-print cloud_name
+def checkout_tag(main_dir, repo_name, branch, tag=None):
+	"""
+	Stash existing repository data and check out specific branch and/or tag
+	"""
+	os.chdir(os.path.join(main_dir,repo_name))
+	git('stash')
+	git('checkout', branch)
+	if tag:
+		git('checkout', 'tags/'+tag)
 
-# Delete existing files
-root_dir = os.path.join(r"\\",cloud_name,"e$",'soundcast_root')
-if os.path.exists(root_dir):
-	shutil.rmtree(root_dir)
+# For each machine, pull tagged version of Soundcast code
+for cloud_name in cloud_list:
+	print cloud_name
 	
-# Download a tagged version of Soundcast code to the cloud machine
-# for year in model_years:
-# 	# Pull code from Github
-# 	cloud_dir = os.path.join(r"\\",cloud_name,"e$","soundcast_root","src")
-# 	git("clone", "https://github.com/psrc/soundcast.git", "-b", 
-# 		soundcast_branch, os.path.join(cloud_dir,str(year)))
+	if update_soundcast:
+		# Download a tagged version of Soundcast code to the cloud machine
+		# for year in model_years:
+		for year in model_years:
+			print year
+			# Pull code from Github
+			cloud_dir = os.path.join(r'\\',cloud_name,'e$','soundcast_root','src')
 
-# 	# Copy batch files
-# 	copyfile(os.path.join(r'batch','run_soundcast_'+str(year)+'.bat'))
+			# create directory if it doesn't exist
+			if not os.path.exists(os.path.join(cloud_dir,str(year))):
+				os.chdir(cloud_dir)
+				# clone repo if it doesn't exist
+				git('clone','https://github.com/psrc/soundcast','-b',
+					soundcast_branch,str(year))
+			
+			os.chdir(os.path.join(cloud_dir,str(year)))
+			git('stash')    # assuming overwrite of existing runs/code
+			git('checkout', soundcast_branch)
+			git('checkout', 'tags/'+soundcast_tag)
 
-# Copy input_configuration to Soundcast runs
+	if update_urbansim:
+		cloud_dir = os.path.join(r'\\',cloud_name,'e$','opusgit')
 
-# Pull Urbansim code
+		# Pull urbansim data
+		if not os.path.exists(os.path.join(cloud_dir,'urbansim')):
+			git('clone','https://github.com/psrc/urbansim','-b',urbansim_branch)
+
+		# os.chdir(os.path.join(cloud_dir,'urbansim'))
+		# git('stash')
+		# git('checkout', urbansim_branch)
+		# git('checkout', 'tags/'+urbansim_tag)
+		checkout_tag(cloud_dir, 'urbansim', urbansim_branch, urbansim_tag)
+
+		# Pull urbansim_data data
+		if not os.path.exists(os.path.join(cloud_dir,'urbansim_data')):
+			git('clone','https://github.com/psrc/urbansim','-b',urbansim_data_branch)
+
+		# Update urbansim_data code, and/or check out tag
+		# os.chdir(os.path.join(cloud_dir,'urbansim_data'))
+		# git('stash')
+		# git('checkout', urbansim_data_branch)
+		# git('checkout', 'tags/'+urbansim_data_tag)
+		checkout_tag(cloud_dir, 'urbansim_data', urbansim_data_branch, urbansim_data_tag)
--- cloud_setup_config.py
+++ cloud_setup_config.py
@@ -1,9 +1,18 @@
 # List of machines to target
-cloud_list = ['aws-model03','aws-model04','aws-model05']
-
-soundcast_branch = 'dev'
+# cloud_list = ['aws-model03','aws-model04','aws-model05']
+cloud_list = ['aws-model04','aws-model05']
 
 # List of Soundcast years to model
 model_years = [2014,2025,2040,2050]
 
+update_soundcast = True
+update_urbansim = True
 
+soundcast_branch = 'master'
+soundcast_tag = 'v2.1'
+
+urbansim_branch = 'dev'
+urbansim_tag = None
+
+urbansim_data_branch = 'dev'
+urbansim_data_tag = None
