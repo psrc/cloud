a31726ba005fce00eaa35fdc1c6881754299d056
bricegnichols <bricenichols@gmail.com>
2018-07-30 14:58:30 -0700

    save log files and copy of ini

--- cloud_setup.py
+++ cloud_setup.py
@@ -3,6 +3,9 @@
 import os, sys
 import subprocess
 import json
+import time
+import datetime
+import socket
 from shutil import copyfile
 import configparser
 
@@ -31,7 +34,10 @@ def config_list(heading, var, config):
 	return [i.strip() for i in raw]
 
 def update_server(config_name):
-# local working directory
+	# Record time
+	ts = time.time()
+
+	# local working directory
 	local_dir = os.getcwd()
 
 	config = configparser.ConfigParser()
@@ -51,18 +57,23 @@ def update_server(config_name):
 		if not os.path.exists(src_dir):
 			os.makedirs(src_dir)
 
+		opusgit_dir = os.path.join(r'\\',cloud_name,'e$','opusgit')
+		if not os.path.exists(opusgit_dir):
+			os.makedirs(opusgit_dir)
+
 		# Proceed through each year if we are updating repositories or copying batch files
-		if (config['global']['update_soundcast_repo'] or 
-			config['global']['update_soundcast_config'] or 
-			config['global']['copy_batch_files']):
+		if (config['global']['update_soundcast_repo'] in['true','True','TRUE'] or 
+			config['global']['update_soundcast_config'] in['true','True','TRUE'] or 
+			config['global']['copy_batch_files'] in['true','True','TRUE']):
 			# Download a tagged version of Soundcast code to the cloud machine
 			
 			model_years = config_list('soundcast', 'model_years', config)
 			for year in model_years:
 				print year
 
-				if config['global']['update_soundcast_repo']:
-					
+				if config['global']['update_soundcast_repo'] in['true','True','TRUE']:
+					print 'Updating Soundcast code from remote repository'
+
 					# clone directory if it doesn't exist
 					if not os.path.exists(os.path.join(src_dir,str(year))):
 						os.chdir(src_dir)
@@ -77,45 +88,66 @@ def update_server(config_name):
 						'yes'
 					checkout_tag(src_dir, str(year), config['soundcast']['branch'], config['soundcast']['tag'])
 
-				if config['global']['update_soundcast_config']:
-					# Copy configuration files to each Soundcast run year
+				if config['global']['update_soundcast_config'] in['true','True','TRUE']:
+					print 'Updating Soundcast configuration files'
+
 					src = os.path.join(local_dir,'config','soundcast',config['soundcast']['config_source'], 
 						str(year),'input_configuration.py')
 					dst = os.path.join(src_dir,str(year),'input_configuration.py')
 					copyfile(src, dst)
 
-				if config['global']['copy_batch_files']:
-					# Copy batch files for integrated run management
+				if config['global']['copy_batch_files'] in['true','True','TRUE']:
+					print 'Copying Soundcast batch files for integrated run management'
+
 					src = os.path.join(local_dir,'batch','run_soundcast_'+str(year)+'.bat')
 					dst = os.path.join(src_dir,'run_soundcast_'+str(year)+'.bat')
 					copyfile(src, dst)
 
 		# Add existing skim files to the root_dir if requested
-		if config['global']['copy_archive_skims']:
+		if config['global']['copy_archive_skims'] in['true','True','TRUE']:
 			skim_years = config_list('skims', 'skim_years', config)
 			for year in skim_years:
+				print 'Copying archived skims for: ' + str(year)
 				src = os.path.join(config['skims']['skims_archive_dir'],str(year)+'-travelmodel.h5')
 				dst = os.path.join(root_dir,str(year)+'-travelmodel.h5')
 				copyfile(src, dst)
 
-		if update_urbansim_repo:
+		if config['global']['update_urbansim_repo'] in['true','True','TRUE']:
+			print 'Updating urbansim from remote repository'
 
-			# Pull urbansim data
-			if not os.path.exists(os.path.join(src_dir,'urbansim')):
-				git('clone','https://github.com/psrc/urbansim','-b',urbansim_branch)
+			if not os.path.exists(os.path.join(opusgit_dir,'urbansim')):
+				git('clone','https://github.com/psrc/urbansim','-b',config['urbansim']['branch'])
 
-			# os.chdir(os.path.join(src_dir,'urbansim'))
-			checkout_tag(src_dir, 'urbansim', urbansim_branch, urbansim_tag)
+			checkout_tag(opusgit_dir, 'urbansim', config['urbansim']['branch'], config['urbansim']['tag'])
 
-		if update_urbansim_data_repo:
-			# Pull urbansim_data data
-			if not os.path.exists(os.path.join(src_dir,'urbansim_data')):
-				git('clone','https://github.com/psrc/urbansim_data','-b',urbansim_data_branch)
+		if config['global']['update_urbansim_data_repo'] in['true','True','TRUE']:
+			print 'Updating urbansim_data from remote repository'
+			
+			if not os.path.exists(os.path.join(opusgit_dir,'urbansim_data')):
+				git('clone','https://github.com/psrc/urbansim_data','-b',config['urbansim_data']['branch'])
 
 			# Update urbansim_data code, and/or check out tag
-			checkout_tag(src_dir, 'urbansim_data', urbansim_data_branch, urbansim_data_tag)
+			checkout_tag(opusgit_dir, 'urbansim_data', config['urbansim_data']['branch'], config['urbansim_data']['tag'])
 
 		# Export copy of config file to local server
+		log_dir = os.path.join(r'\\',cloud_name,'e$','log')
+		if not os.path.exists(log_dir):
+			os.makedirs(log_dir)
+
+		src = os.path.join(local_dir,config_name)
+		dst = os.path.join(os.path.join(log_dir, config_name.split('\\')[-1]))
+		print src
+		print dst
+		copyfile(src, dst)
+
+		# Write a log file with info about this process
+		sttime = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d | %H:%M:%S')
+		logfile = os.path.join(log_dir, 'log.txt')
+		with open(logfile, 'w') as f:
+			f.write('Config input: ' + config_name + '\n')
+			f.write('Script initiated: ' + sttime + '\n')
+			f.write('Hostname: ' + socket.gethostname() + '\n')
+			f.write('Hostname directory: ' + local_dir)
 
 def parse_config(config_name):
 
